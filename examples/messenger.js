edf8a3f9e27b0f0d7f9d3a20fd2841f1597fbdca
Charly Riviere <crw.riviere@gmail.com>
2016-11-19 11:11:22 +0000

    stuff

--- examples/messenger.js
+++ examples/messenger.js
@@ -140,20 +140,14 @@ const actions = {
       return resolve(context);
     });
   },
-  getSpendings({context,entities}){
+   getSpendings({context,entities}){
         return new Promise(function(resolve, reject) {
-      // var location = firstEntityValue(entities, 'location')
-      // if (location) {
-      //   context.forecast = 'sunny in ' + location; // we should call a weather API here
-      //   delete context.missingLocation;
-      // } else {
-      //   context.missingLocation = true;
-      //   delete context.forecast;
-      // }
-      context.spendingCategory = entities.spendingCategory;
+      var spendingCategory = firstEntityValue(entities, 'spendingCategory')
+      
+      context.spendings = '$12121 on '+spendingCategory;
       return resolve(context);
     });
-  }
+  },   
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
 };
